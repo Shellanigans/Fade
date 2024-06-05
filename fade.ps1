b5a896a0ceaee1d70830d101761f6903dfd6d4b7
TastyKayKs <KevinKay1990@Gmail.com>
2024-06-04 23:23:57 -0500

    Update fade.ps1

--- fade.ps1
+++ fade.ps1
@@ -3,7 +3,6 @@
     # This would mean you have to kill it in taskmgr though
         # Maybe another hot-key to end it?
 
-# TODO Add simple way to change the fade value
 # TODO Profile how much CPU usage changes when using different delays
 
 Add-Type -TypeDefinition @"
@@ -34,17 +33,28 @@ public static class fade{
 }
 "@
 
+$fadeValue = 128 # Ranges from 0 (invisible) to 255 (normal, opaque)
+$settings = @{}
 while($true){
     if([fade]::GetKeyState(0x13) -gt 1){
         $fg = [fade]::GetForegroundWindow()
         # Find window with focus
-        $prevSettings = [fade]::GetWindowLongPtrA($fg,-20)
-        # Get the extended window settings 
+
+        $prevSettings = 0
+        if($settings.Keys.Contains($fg)){ # If previously seen window
+            $prevSettings = $settings.$fg
+            # Recall saved settings
+        }else{
+            $prevSettings = [fade]::GetWindowLongPtrA($fg,-20)
+            # Get the extended window settings
+            $settings.$fg = $prevSettings
+            # Store them in the hashtable
+        }
 
         if($prevSettings -bxor 0x80000){
             [void][fade]::SetWindowLongPtrA($fg, -20, ($prevSettings -bor 0x80000))
         }
-        # Pause/break pressed so at the BARE minimum we need to force the layered state to check alpha
+        # Pause/break pressed so at a minimum we need to force the layered style to check alpha
 
         $colorKey = 0
         $alpha = 255
@@ -52,11 +62,11 @@ while($true){
         $retVal = [fade]::GetLayeredWindowAttributes($fg, [ref]$colorKey, [ref]$alpha, [ref]$flags)
         # This is a pass by ref function so the variables need to exist first and their value gets updated
 
-        if($retVal){
+        if($retVal){ # If call succeeded
             if($alpha -eq 0){$alpha = 255} # The first call will succeed but alpha is always 0 in that case. IDK
             
-            $newAlpha = 128
-            if($alpha -eq 128){$newAlpha = 255} # Makes the values between alpha and newAlpha opposites
+            $newAlpha = $fadeValue
+            if($alpha -eq $fadeValue){$newAlpha = 255} # Makes the values between alpha and newAlpha opposites
 
             [void][fade]::SetLayeredWindowAttributes($fg, 0, $newAlpha, 2)
 
@@ -72,7 +82,7 @@ while($true){
 
             $retVal = [fade]::GetLayeredWindowAttributes($fg, [ref]$colorKey, [ref]$alpha, [ref]$flags)
             # Last check on current alpha value
-            if($retVal -and $alpha -eq 255){ # If alpha = 255, then we must be done with it, change the type back
+            if($retVal -and $alpha -eq 255){ # If alpha = 255, then we must be done with it, change the style back
                 [void][fade]::SetWindowLongPtrA($fg, -20, $prevSettings)
             }
         }
